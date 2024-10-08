3366b580e6cd7f63c6c5a256d77c8e601e0a3fba
Vivek Pradhan <thepradhan4u@gmail.com>
2024-10-08 17:59:14 +0530

    Basics and projects of React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+function Vivek() {
+  return (
+    <h1>Hello React and Vivek</h1>
+  );
+  
+}
+
+export default Vivek;
+
---
+++ 01basicreact/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Vivek from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    <Vivek />
+  
+);
